53833ba3d21d4b95e4d52383c605b2fa1b63241a
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-04-20 20:50:52 -0400

    cambios en micuenta y javascript

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,37 +2,41 @@
 //VALIDACION micuenta.html FORMULARIO HTML
 
 function Ingreso(){
-    var user,password,chk;
-    user = document.getElementById("valida_usuario").value;
-    password = document.getElementById("valida_contraseña").value;
-    chk = document.getElementById("valida_checkbox").value;
+    var user,password,chk,txt;
     
-    if (user){
-
+    user = document.getElementById("username").value;
+    password = document.getElementById("pwd").value;
+    chk = document.getElementById("chkHtml").value;
+    
+    //USUARIO
+    if (user.length == 0){
+        txt = 'Usuario no debe estar vacío';
+    }else{
+        txt = "";
     }
+    document.getElementById("valida_usuario").innerHTML = txt;
+
+    //CONTRASEÑA
+    if (password.length == 0){
+        txt = "Escriba una contraseña"; 
+    }else if (password.length > 1 && password.length < 6){
+        txt = "Contraseña muy corta";
+    }else{
+        txt ="";
+    }
+    document.getElementById("valida_contraseña").innerHTML = txt;
+
+    
+    //CHECKBOX
+   if (chk = true){
+    txt = "sesión abierta";
+   }else{
+    txt = "";
+   }
+   document.getElementById("valida_checkbox").innerHTML = txt
 }
 
 
 
 
 
-//FUNCION EJEMPLO
-
-function validar(){
-    var usu,pass,text;
-    usu = document.getElementById("correo").value;
-    pass = document.getElementById("password").value;
-
-    if (usu.length == 0){
-        text = "Usuario no debe estar vacío";   
-    }else{
-        text = "";
-    }
-    document.getElementById("valida_email").innerHTML = text;
-    if (pass.length != 8){
-        text = "Contraseña debe tener largo 8";
-    }else{
-        text = "";
-    }
-    document.getElementById("valida_password").innerHTML = text;
-}
