9eb7bb76c8365ccff28edc40851fbab706b543ba
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-05-07 15:53:25 -0400

    Agregado script api y archivos json

--- Javascript/jquery.js
+++ Javascript/jquery.js
@@ -15,3 +15,20 @@ $(document).ready(function(){
         $("#form-contacto").valid()
     });
 });
+
+
+//INTEGRACIÓN DE API RESCATANDO INFORMACIÓN
+//INTEGRACIÓN DE API RESCATANDO INFORMACIÓN
+//NOTA: este jquery fue sacado de apiComidas ejercicio YARAVI
+$(document).ready(function(){
+    $("#enviar").click(function(){
+        $.get("https://nosotros-ce6c2-default-rtdb.firebaseio.com/Empresas.json",
+        function(data){
+            $.each(data.Empresas, function(i,item){
+                $("#categorias").append("<tr><td>"+item.idCategory+"</td><td>"+
+                item.strCategory + "</td><td><img src='" + item.strCategoryThumb + "'></td><td>" +
+                item.strCategoryDescription + "</td></tr>");
+            });
+        });
+    });
+});
