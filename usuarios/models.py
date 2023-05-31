03c48e29dadcab798224881df2150b677b72c7cd
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-05-31 19:38:44 -0400

    agregado claves autoincrementables

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # modelo de la tabla USUARIO
 class Usuarios(models.Model):
-    id_usuario       = models.CharField(primary_key=True, max_length=10)
+    id_usuario       = models.BigAutoField(primary_key=True)
     nombre           = models.CharField(max_length=20)
     apellidos        = models.CharField(max_length=50)
     email            = models.EmailField(unique=True, max_length=100)
