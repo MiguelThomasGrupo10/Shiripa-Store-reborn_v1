96729a07f82095d2d818f5ee2e8299bd3557dbc0
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-06-22 18:50:43 -0400

    commit

--- venta/models.py
+++ venta/models.py
@@ -22,21 +22,6 @@ class Plataforma(models.Model):
 
 #Modelo de la tabla VENTA
 
-class Categoria(models.Model):
-    Id_categoria     = models.AutoField(db_column='idCategoria', primary_key=True)
-    categoria        = models.CharField(unique = True, max_length=50)
-    
-    def __str__(self):
-        return str(self.categoria) 
-
-class Plataforma(models.Model):
-    Id_plataforma    = models.AutoField(db_column='idPlataforma',primary_key=True)
-    plataforma       = models.CharField(unique = True, max_length=50)
-
-    def __str__(self):
-        return str(self.plataforma) 
-
-
 class Inventario(models.Model):
     DISPONIBILIDAD_CHOICES = (
         ('disponible', 'Disponible'),
@@ -54,37 +39,26 @@ class Inventario(models.Model):
     def __str__(self):
         return str(self.nombre_juego)+" "+str(self.plataforma)
 
-#FUNCION LICENCIA ALEATORIA, realiza una funcion aleatoria que utiliza después la boleta
-def generar_codigo_licencia():
-    caracteres_permitidos = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    longitud_codigo = 10
-    codigo_licencia = ''.join(random.choice(caracteres_permitidos) for _ in range(longitud_codigo))
-    return codigo_licencia
+# #FUNCION LICENCIA ALEATORIA, realiza una funcion aleatoria que utiliza después la boleta
+# def generar_codigo_licencia():
+#     caracteres_permitidos = string.ascii_uppercase + string.ascii_lowercase + string.digits
+#     longitud_codigo = 10
+#     codigo_licencia = ''.join(random.choice(caracteres_permitidos) for _ in range(longitud_codigo))
+#     return codigo_licencia
 
-<<<<<<< HEAD
-# Obtener el nombre y valor del juego desde el Inventario hacia Boleta
-def save(self, *args, **kwargs):
-    inventario = Inventario.objects.get(Id_juego=self.Id_inventario_id)
-    self.nombre_juego = inventario.nombre_juego
-    self.valor = inventario.valor
-    super().save(*args, **kwargs)    
+# <<<<<<< HEAD
+# # Obtener el nombre y valor del juego desde el Inventario hacia Boleta
+# def save(self, *args, **kwargs):
+#     inventario = Inventario.objects.get(Id_juego=self.Id_inventario_id)
+#     self.nombre_juego = inventario.nombre_juego
+#     self.valor = inventario.valor
+#     super().save(*args, **kwargs)    
     
-# Obtener el email desde el Usuario hacia Boleta     
-def save(self, *args, **kwargs):   
-    usuario = Usuarios.objects.get(id_usuario=self.Id_usuario_id)
-    self.email = usuario.email
-    super().save(*args, **kwargs)
-
-class Boleta(models.Model):
-    Id_boleta     = models.BigAutoField(primary_key=True)
-    Id_usuario    = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    Id_inventario = models.ForeignKey(Inventario, on_delete=models.CASCADE)
-    nombre_juego  = models.CharField(max_length=30)
-    valor         = models.DecimalField(max_digits=8, decimal_places=2)
-    licencia      = models.CharField(max_length=10, default=generar_codigo_licencia) #aquí se utiliza la función
-    email         = models.EmailField()
-=======
-
+# # Obtener el email desde el Usuario hacia Boleta     
+# def save(self, *args, **kwargs):   
+#     usuario = Usuarios.objects.get(id_usuario=self.Id_usuario_id)
+#     self.email = usuario.email
+#     super().save(*args, **kwargs)
 
 
 # class Boleta(models.Models):
@@ -95,14 +69,10 @@ class Boleta(models.Model):
 #     valor         = models.DecimalField(max_digits=8, decimal_places=2)
 #     licencia      = models.CharField(max_length=10, default=generar_codigo_licencia) #aquí se utiliza la función
 #     email         = models.EmailField()
->>>>>>> origin/master
+
 
 #     def __str__(self):
 #         return str(self.Id_boleta)
 
-<<<<<<< HEAD
     
 
-=======
-    
->>>>>>> origin/master
